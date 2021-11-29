761be76fef048b41ae739007c55b7591afb950ee
muna <munaa98@gmail.com>
2021-11-29 20:52:33 +0000

    Task 3 done. Hi muna In 5 years time, you will be 25

--- session-3/index.js
+++ session-3/index.js
@@ -28,3 +28,26 @@ function conversation() {
 
 }
 conversation();
+
+
+// var x = "muna";
+// var y = 24;
+// c = y + 5;
+
+// function futureAge (x,y) {
+// return `Hi ${x} In 5 years time, you will be ${c}` 
+// // return "Hi " +  x + " In 5 years time, you will be " +c;
+// }
+// console.log(futureAge (x,y));
+
+
+// The top part is the way i did it First. It makes more sense at the bottom
+
+
+function futureAge (name,age) {
+    const ageInFiveYears = age + 5;
+    return `Hi ${name} In 5 years time, you will be ${ageInFiveYears}`
+}    
+
+console.log(futureAge("muna", 20));
+    
